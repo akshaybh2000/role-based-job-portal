ba7e028cd6aed145ccb7d3d456c1f6477b6adf42
akshaybh2000 <adbharati2000@gmail.com>
2026-07-11 13:31:43 +0530

    Fix ESLint warnings

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { fetchData } from "../apiConfig";
+// import { fetchData } from "../apiConfig";
 import "../index.css";
 
 export const title = {
